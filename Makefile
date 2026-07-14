cd841cbb13bd7709f63f25c4ff85868ea55f87d9
anirudhamudhan <anirudh.amudhan2008@gmail.com>
2026-07-13 23:04:36 -0400

    Add automated note publishing workflow

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: export
+
+export:
+	emacsclient -e '(load-file "~/Documents/Fall-2026-Notes/publish.el")'
---
+++ publish.el
@@ -0,0 +1,4 @@
+(org-publish "notes-html" t)
+(org-publish "notes-pdf" t)
+
+(message "Finished publishing notes!")
