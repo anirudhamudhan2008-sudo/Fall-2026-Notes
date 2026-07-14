a90a2430de673740894bc8b116410f01808ed9bd
anirudhamudhan <anirudh.amudhan2008@gmail.com>
2026-07-14 14:46:53 -0400

    Add course navigation and website styling

--- publish.el
+++ publish.el
@@ -2,6 +2,9 @@
 (require 'ox)
 (require 'ox-publish)
 
+(setq org-html-head
+      "<link rel=\"stylesheet\" href=\"../../css/style.css\">")
+
 (org-publish "notes-html" t)
 
 (message "Finished publishing website!")
