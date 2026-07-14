bc71f4da0e8a7f456adf3f6b3ac6df5d02cf580d
anirudhamudhan <anirudh.amudhan2008@gmail.com>
2026-07-14 14:41:24 -0400

    Clean final note structure

--- publish.el
+++ publish.el
@@ -1,9 +1,7 @@
 (require 'org)
 (require 'ox)
 (require 'ox-publish)
-(require 'ox-latex)
 
 (org-publish "notes-html" t)
-(org-publish "notes-pdf" t)
 
-(message "Finished publishing notes!")
+(message "Finished publishing website!")
