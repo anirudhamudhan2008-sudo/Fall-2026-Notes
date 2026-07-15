dff5587babef59477964471013919d0f93949767
anirudhamudhan <anirudh.amudhan2008@gmail.com>
2026-07-15 09:47:08 -0400

    Fix publishing system and add course templates
    - Fix publish.el with proper org-publish-project-alist
    - Add HTML, PDF, and CSS publishing projects
    - Add LaTeX packages (mhchem, chemfig, tikz) for STEM courses
    - Update Makefile to use --batch mode (fixes Doom Emacs hang)
    - Populate all 6 course .org files with structured templates
    - Update README.org with usage instructions and directory structure
    - Add GitHub Actions workflow for automated publishing
    - Update .gitignore to exclude build artifacts

--- Makefile
+++ Makefile
@@ -1,5 +1,37 @@
-.PHONY: export
+.PHONY: all html pdf clean serve
 
-export:
-	emacsclient -e '(load-file "~/Documents/Fall-2026-Notes/publish.el")'
+# Default target
+all: html pdf
+
+# HTML publishing
+html:
+	@echo "Publishing HTML..."
+	emacs --batch -l ~/Documents/Fall-2026-Notes/publish.el \
+	  --eval '(org-publish "notes-html" t)'
 	cp docs/README.html docs/index.html
+	@echo "HTML publishing complete!"
+
+# PDF publishing
+pdf:
+	@echo "Publishing PDFs..."
+	emacs --batch -l ~/Documents/Fall-2026-Notes/publish.el \
+	  --eval '(org-publish "notes-pdf" t)'
+	@echo "PDF publishing complete!"
+
+# Clean build artifacts
+clean:
+	@echo "Cleaning build artifacts..."
+	rm -rf docs/Courses/*/index.html
+	rm -rf PDFs/Courses/*/index.pdf
+	rm -f docs/index.html
+	@echo "Clean complete!"
+
+# Start Emacs server for publishing
+serve:
+	emacs --daemon &
+	@echo "Emacs server started!"
+
+# Publish and serve locally
+publish: all
+	@echo "Starting local server..."
+	cd docs && python3 -m http.server 8000
--- publish.el
+++ publish.el
@@ -6,10 +6,73 @@
 (setq org-html-head-include-default-style nil)
 (setq org-html-head-include-scripts nil)
 
-;; Use our stylesheet
+;; Use our stylesheet (relative path for GitHub Pages)
 (setq org-html-head
-      "<link rel=\"stylesheet\" href=\"/Fall-2026-Notes/css/style.css\" />")
+      "<link rel=\"stylesheet\" href=\"../css/style.css\" />")
 
-(org-publish "notes-html" t)
+;; Define publishing projects
+(setq org-publish-project-alist
+      '(
+        ;; HTML publishing
+        ("notes-html"
+         :base-directory "~/Documents/Fall-2026-Notes"
+         :publishing-directory "~/Documents/Fall-2026-Notes/docs"
+         :base-extension "org"
+         :publishing-function org-html-publish-to-html
+         :recursive t
+         :exclude "README\\.org"
+         :auto-sitemap t
+         :sitemap-title "Fall 2026 Notes"
+         :sitemap-filename "index.org"
+         :sitemap-sort-folders t)
 
-(message "Finished publishing website!")
+        ;; PDF publishing
+        ("notes-pdf"
+         :base-directory "~/Documents/Fall-2026-Notes"
+         :publishing-directory "~/Documents/Fall-2026-Notes/PDFs"
+         :base-extension "org"
+         :publishing-function org-latex-publish-to-pdf
+         :recursive t
+         :exclude "README\\.org")
+
+        ;; CSS publishing
+        ("notes-css"
+         :base-directory "~/Documents/Fall-2026-Notes/docs/css"
+         :publishing-directory "~/Documents/Fall-2026-Notes/docs/css"
+         :base-extension "css"
+         :publishing-function org-publish-attachment)
+
+        ;; Combined project
+        ("notes" :components ("notes-html" "notes-pdf" "notes-css"))
+      ))
+
+;; Export settings
+(setq org-export-with-toc t)
+(setq org-export-with-section-numbers t)
+(setq org-export-with-author "Anirudh Amudhan")
+(setq org-export-with-date t)
+
+;; PDF export settings with LaTeX packages for chemistry/biology
+(setq org-latex-pdf-process
+      '("pdflatex -interaction=nonstopmode -output-directory=%o %f"
+        "pdflatex -interaction=nonstopmode -output-directory=%o %f"))
+
+;; LaTeX class with chemistry/biology packages
+(setq org-latex-classes
+      '(("article"
+         "\\documentclass[11pt]{article}
+\\usepackage{mhchem}
+\\usepackage{chemfig}
+\\usepackage{tikz}
+\\usepackage{amsmath}
+\\usepackage{graphicx}
+\\usepackage{hyperref}
+\\usepackage[margin=1in]{geometry}"
+         ("\\section{%s}" . "\\section*{%s}")
+         ("\\subsection{%s}" . "\\subsection*{%s}")
+         ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
+
+;; Set default LaTeX class
+(setq org-latex-default-class "article")
+
+(message "Publishing projects configured!")
