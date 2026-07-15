38f4bfcd3b380f5931e01133a9c04e6f681c7890
anirudhamudhan <anirudh.amudhan2008@gmail.com>
2026-07-15 08:51:22 -0400

    Update website

--- publish.el
+++ publish.el
@@ -2,8 +2,13 @@
 (require 'ox)
 (require 'ox-publish)
 
+;; Don't use Org's built-in CSS
+(setq org-html-head-include-default-style nil)
+(setq org-html-head-include-scripts nil)
+
+;; Use our stylesheet
 (setq org-html-head
-      "<link rel=\"stylesheet\" href=\"../../css/style.css\">")
+      "<link rel=\"stylesheet\" href=\"/Fall-2026-Notes/css/style.css\" />")
 
 (org-publish "notes-html" t)
 
